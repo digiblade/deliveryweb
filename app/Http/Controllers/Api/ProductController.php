2a1f7937f62bb0ff878ecd21080e8e4b2afa532a
digiblade <ak.chourasia.007@gmail.com>
2021-07-18 08:45:11 +0530

    api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -53,7 +53,7 @@ class ProductController extends Controller
         return $data;
     }
     public function getProductAPI(Request $req,$id){ 
-        $data = ProductModel::where("product_companyid","=",$id)->orderBy('id','desc')->get();
+        $data = ProductModel::where("product_companyid","=",$id)->orderBy('id','desc')->with('category')->get();
         return $data;
     }
     
