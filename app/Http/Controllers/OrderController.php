39e6053ed19aec080a270f72f36d57515cfb2c6c
digiblade <ak.chourasia.007@gmail.com>
2021-09-10 20:56:47 +0530

    stock

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -50,7 +50,7 @@ class OrderController extends Controller
                 // return $stock;
                 $man["stock"] = 0;
                 foreach($stock as $res){
-                    echo ( ($res->manufacturing_totalcount - $res->manufacturing_sold));
+                    echo ( $res->manufacturing_totalcount );
                     if($man["stock"]< $req->qty){
                         if($req->qty > ($res->manufacturing_totalcount - $res->manufacturing_sold)){
                             echo "1st";
